2f8bd6257e15220faf88d0b83acc1530adfa532f
SebasChicaiza <sebastian.erazo275@gmail.com>
2025-12-27 17:11:15 -0500

    fix expiration headers

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,19 @@ const nextConfig: NextConfig = {
   async redirects() {
     return redirects;
   },
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|jpeg|png|webp|gif|ico|woff2|css)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
